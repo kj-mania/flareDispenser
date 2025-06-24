c799df69e6151ca7c1fe4e6f75f0b05a7201fa6a
kj <kjandrews1@hotmail.com>
2025-06-24 11:21:48 -0400

    hellodfalsljfasl

---
+++ test.py
@@ -0,0 +1,50 @@
+import RPi.GPIO as GPIO
+import time
+
+class Light:
+    def __init__(self):
+        self.outputs = [21]
+        
+        GPIO.setmode(GPIO.BCM)
+
+        for pin in self.outputs:
+            GPIO.setup(pin, GPIO.OUT)
+            GPIO.output(pin, GPIO.LOW)
+
+    def lightOutputs(self):
+        for pin in self.outputs:
+            GPIO.output(pin, GPIO.LOW)
+        print("Lighting outputs...")
+        for pin in self.outputs:
+            print(f"Turning on output on pin {pin}")
+            GPIO.output(pin, GPIO.LOW)
+            time.sleep(5)
+
+    def checkAllInputs(self) -> bool:
+        flaresWentOff = 0
+        for pin in self.inputs:
+            print(f"Checking input on pin {pin}")
+            if GPIO.input(pin) == GPIO.HIGH:
+                flaresWentOff += 1
+                print(f"Input on pin {pin} is HIGH, flare went off!")
+        return flaresWentOff == len(self.inputs)
+
+    @staticmethod
+    def main():
+        light = Light()
+        light.lightOutputs()
+        # light.checkAllInputs()
+    
+if __name__ == "__main__":
+    try:
+        print("Initializing light system...")
+        print("starting in 5 seconds...")
+        time.sleep(5) 
+        Light.main()
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Program interrupted by user.")
+    finally:
+        GPIO.cleanup()
+
