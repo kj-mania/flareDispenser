92425be817cb1e33c8f6fa965d03367ccf8ea656
KJ Andrews <kjandrews@Braydens-IPhone.local>
2025-06-23 23:54:58 -0400

    add files

---
+++ flareSystem.py
@@ -0,0 +1,43 @@
+import time
+from typing import List
+from gpio import Light
+
+class FlareSystem:
+
+    def __init__(self):
+        self.delay = 3
+
+    def flareSuccess(self, outcomes: list[bool]) -> bool:
+        sum = 0
+        for outcome in outcomes:
+            if outcome:
+                success += 1
+        return success == len(outcomes)
+    
+    def fireFlare(self, index: int, light: Light) -> bool:
+        flareOut = light.outputs[index]
+        flareToReturn = light.inputs[index]
+        light.outputOn(flareOut)
+        return light.inputConfirm(flareToReturn)
+    
+    def signalSuccess(self, success: bool, light: Light, red: int, green: int):
+        if success:
+            light.activate(green)
+        else:
+            light.activate(red)
+
+    
+
+    
+
+    
+
+
+
+
+
+
+    
+
+
+    
