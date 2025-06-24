b5d945c06cc4ee7f708296d1acb83113c09fb4ca
kj <kjandrews1@hotmail.com>
2025-06-24 15:58:19 -0400

    hellald

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from light import Light
 from flareSystem import FlareSystem
 import time as t
 
-outputs = [21, 20, 16]
-inputs = [19, 13, 6]
+outputs = [21, 20, 16, 12]
+inputs = [19, 13, 6, 5]
 light = Light(outputs, inputs)
 
 def run():
