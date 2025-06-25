2e7764045b006c3605d00993c795770fe597b6c0
kj <kjandrews1@hotmail.com>
2025-06-25 13:05:03 -0400

    asdkjfasd

--- inputManager.py
+++ inputManager.py
@@ -27,7 +27,7 @@ class Order:
     
     def getOrder(self):
         newOrder = []
-        for i in range (len(self.order)):
+        for i in range (len(self.dicts)):
             x = input("Input order one at a time:")
             newOrder.append(int(x))
             
